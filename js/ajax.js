2ac6c5fb44be9f5bdff70c7a404dddb2021c81c6
Khrapov Denys <khrapov.denys@gmail.com>
2022-06-05 14:09:38 +0300

    ajax

---
+++ js/ajax.js
@@ -0,0 +1,26 @@
+//ajax и общение с сервером
+const inputUAH = document.querySelector('#uah'),
+	inputUSD = document.querySelector('#usd');
+
+
+inputUAH.addEventListener('input', () => {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', 'js/current.json');
+	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	request.send();
+
+	request.addEventListener('load', () => {
+		if (request.status === 200) {
+			const data = JSON.parse(request.response);
+			inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(2);
+		} else {
+			inputUSD.value = "Что-то пошло не так :(";
+		}
+	});
+
+	//status
+	//statusText
+	// response
+	// readyState 
+});
